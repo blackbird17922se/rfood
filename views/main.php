687c570838192740cb3ddb434f01344e49e0e666
blackbird17922se <blackbird17922@gmail.com>
2022-04-13 00:26:56 -0500

    notas cliente

--- views/main.php
+++ views/main.php
@@ -27,9 +27,9 @@ if(!empty($_SESSION['rol']==1 || $_SESSION['rol']==2 || $_SESSION['rol']==4 || $
             <div class="col-sm-10">
               
               <h1>RFood</h1>
-              <p>DsD RFood para Windows 98</p>
+              <p>DsD RFood para Windows XP</p>
               <!-- Num version princip,Numero cuando hay nuevos cambios,Año,mes,dia,hora -->
-              <p><i><b>RFood Milestone 2</b> <br> Version de pruebas.Build 3.0.220408-0000</i></p>
+              <p><i><b>RFood Milestone 3</b> <br> Version de pruebas.Build 4.0.220413-0018</i></p>
               <p>Copyright &copy; 2020-2022 DsD Desarrollos Dinámicos</p>
               <br>
               <p>Se autoriza el uso de este producto a:</p>
@@ -37,6 +37,22 @@ if(!empty($_SESSION['rol']==1 || $_SESSION['rol']==2 || $_SESSION['rol']==4 || $
               <p><i>Id. Del producto: 32644-072-01808804</i></p>
               <br>
               <h4>Novedades</h4>
+              <h5>13/04/2022 (Build 4.0.220413-0018)</h5>
+              <p>
+                - Agrega nuevo diseño en modulos de orden y ventas. ahora se puede generar <br>
+                  las ventas al visualizar todas las mesas registradas, cuando la mesa este <br>
+                  disponible, se dispone del boton 'Nueva orden' para crear una nueva orden a la mesa. <br>
+                  Cuando la mesa esta con una orden, se vusializa el id de la orden y dos botones <br>
+                  Uno para ver los productos de dicha orden y otro para editar esa orden. <br>
+                  <br>
+                  - El modulo de caja permite ver las mesas que tienen pedidos pendientes por cancelar,<br>
+                  solo se debe presionar el boton 'seleccionar' y cargara los items y costos a cobrar de <br>
+                  dicha mesa.
+              </p>
+              
+
+              <h5>Anteriores Mejoras</h5>
+
               <h5>08/04/2022 (Build 2.4.220408-0000)</h5>
               <p>
                 - Optimizacion interna del sistema. <br>
@@ -47,7 +63,6 @@ if(!empty($_SESSION['rol']==1 || $_SESSION['rol']==2 || $_SESSION['rol']==4 || $
                   &nbsp &nbsp al arreglar algunos titulos, textos y diseño.
               </p>
 
-              <h5>Anteriores Mejoras</h5>
               <p>
                 - Agrega venta del dia al momento de salir<br>
                 - Agrega la sección descuadre de inventario (cuando se realizó una venta, pero un ingrediente estaba con 0 stock)<br>
