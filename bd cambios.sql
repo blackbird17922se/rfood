f7feec07cd3fd551185e67a17e64cbdd8952054c
blackbird17922se <blackbird17922@gmail.com>
2022-09-10 17:28:53 -0500

    ag scrip det pedido

--- bd cambios.sql
+++ bd cambios.sql
@@ -1,4 +1,5 @@
 ------BUILD 4.5.1.0
+ALTER TABLE `det_pedido` ADD `prod_pagado` INT(2) NOT NULL ;
 ALTER TABLE `det_pedido` ADD `cant_cuenta_dividida` INT(200) NOT NULL ;
 UPDATE `det_pedido` SET `cant_cuenta_dividida`=`det_cant` WHERE `cant_cuenta_dividida` = 0
 
